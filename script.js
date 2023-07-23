27a3e0f7368870f9654a0cb01d08da75e8815d91
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-23 14:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+let isDragging = false;
+    let dragStartX = 0;
+    let container = document.querySelector(".container");
+    let cubes = document.querySelectorAll(".cube");
+
+    container.addEventListener("mousedown", (e) => {
+      isDragging = true;
+      dragStartX = e.clientX;
+    });
+
+    container.addEventListener("mousemove", (e) => {
+      if (!isDragging) return;
+      const dragDistance = e.clientX - dragStartX;
+      cubes.forEach((cube, index) => {
+        cube.style.transform = `rotateY(${dragDistance / 2}deg)`;
+        cube.style.transition = "transform 0s";
+      });
+    });
+
+    container.addEventListener("mouseup", () => {
+      isDragging = false;
+      cubes.forEach((cube) => {
+        cube.style.transition = "transform 0.5s";
+        cube.style.transform = "rotateY(0)";
+      });
+    });
